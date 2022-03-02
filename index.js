3b5dc2327114f77b9828d18f1fe6d1d1ee02852c
Vitor Bernardo <78373768+vitor-bernardo@users.noreply.github.com>
2022-03-02 12:45:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const select = document.querySelector('select');
-const label = document.querySelector('option');
+const sim = document.querySelector('#sim');
+const nao = document.querySelector('#nao');
+
+sim.addEventListener('change', adicionarHTML);
+nao.addEventListener('change', adicionarHTML);
+
+document.getElementById("btn").style.display = "none";
+document.getElementById("mensagem").style.display = "none";
 
 
-select.addEventListener('change', selecioneQualAmbiente);
+function adicionarHTML() {
+  if (document.getElementById("sim").checked) {
+    document.getElementById("casoNao").style.display = "block";
+    document.getElementById("btn").style.display = "flex";
+    document.getElementById("casoSim").innerHTML = "Digite aqui o que precisa ser comprado: ";
+    document.getElementById("mensagem").style.display = "flex";
 
-function selecioneQualAmbiente(){
-    const choice = select.value;
+  } else {
+    document.getElementById("casoNao").style.display = "none";
+    document.getElementById("casoSim").innerHTML = ""
+    document.getElementById("btn").style.display = "flex";
+    document.getElementById("mensagem").style.display = "none";
 
-    if (choice === 'Terceiros') {
-        label.textContent = 'Escreva abaixo em qual ambiente será essa manutenção:';
-
-    } else {
-        label.textContent = '';
-    }
+  }
 }
